7800a2e15fc5f542bb624792b338e386ce5ca0c5
juninhocb <juninhocb@hotmail.com>
2023-06-19 16:44:42 -0300

    ass2

--- brewery/src/main/java/com/carlosjr/brewery/services/CustomerServiceImpl.java
+++ brewery/src/main/java/com/carlosjr/brewery/services/CustomerServiceImpl.java
@@ -1,10 +1,12 @@
 package com.carlosjr.brewery.services;
 
 import com.carlosjr.brewery.web.model.CustomerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class CustomerServiceImpl implements CustomerService {
     @Override
@@ -14,4 +16,22 @@ public class CustomerServiceImpl implements CustomerService {
                 .name("Petterson")
                 .build();
     }
+
+    @Override
+    public CustomerDto createCustomer(CustomerDto customerDto) {
+        return CustomerDto.builder()
+                .id(UUID.randomUUID())
+                .build();
+    }
+
+    @Override
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+        //TODO UPDATE
+    }
+
+    @Override
+    public void deleteCustomer(UUID customerId) {
+      log.info("This method will delete a resource.");
+    }
+
 }
--- brewery/src/main/java/com/carlosjr/brewery/web/controller/CustomerController.java
+++ brewery/src/main/java/com/carlosjr/brewery/web/controller/CustomerController.java
@@ -2,12 +2,10 @@ package com.carlosjr.brewery.web.controller;
 
 import com.carlosjr.brewery.services.CustomerService;
 import com.carlosjr.brewery.web.model.CustomerDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -24,5 +22,25 @@ public class CustomerController {
     @GetMapping(value = "/{customerId}")
     public ResponseEntity<CustomerDto> getCustomerById(@PathVariable(value = "customerId") UUID customerId){
         return new ResponseEntity<>(customerService.getCustomerById(customerId), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity handlePost(@RequestBody CustomerDto customerDto){
+        CustomerDto createdObject = customerService.createCustomer(customerDto);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", String.format("/api/v1/customer/%d", createdObject.getId()));
+        return new ResponseEntity(headers, HttpStatus.CREATED);
+    }
+
+    @PutMapping(value = "/{customerId}")
+    public ResponseEntity handlePut(@PathVariable(value = "customerId") UUID customerId, @RequestBody CustomerDto customerDto){
+        customerService.updateCustomer(customerId, customerDto);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping(value = "/{customerId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCustomer(@PathVariable(value = "customerId") UUID customerId){
+        customerService.deleteCustomer(customerId);
     }
 }
