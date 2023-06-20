2a1e319a99c5408936acf533fed0532c044e810a
juninhocb <juninhocb@hotmail.com>
2023-06-20 13:19:54 -0300

    create project - closes #3

---
+++ brewry-client/src/main/java/com/carlosjr/brewryclient/BrewryClientApplication.java
@@ -0,0 +1,13 @@
+package com.carlosjr.brewryclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrewryClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrewryClientApplication.class, args);
+	}
+
+}
---
+++ brewry-client/src/test/java/com/carlosjr/brewryclient/BrewryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlosjr.brewryclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrewryClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
