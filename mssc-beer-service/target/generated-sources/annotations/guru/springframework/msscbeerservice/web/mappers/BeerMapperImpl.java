4c1d82a6273572435dafb6b7c32cad807e576a3f
juninhocb <juninhocb@hotmail.com>
2023-06-27 08:12:43 -0300

    jms configuration

--- mssc-beer-service/target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
+++ mssc-beer-service/target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-06-24T10:11:22-0300",
+    date = "2023-06-26T16:44:02-0300",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.9.1 (AdoptOpenJDK)"
 )
 @Component
